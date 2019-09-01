11666300416be78a0d5e9bada68265d1594aacc9
MAC_Herb <hewang@mail.bnu.edu.cn>
2019-09-01 17:54:29 +0800

    New structure of COMP & Hostby func.

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '2c445f2022'
-
-    ADDRESS_COMP = 'http://174.137.53.253:30500/api/competition/all-competitions'
+    ADDRESS_COMP = 'http://45.78.76.192:30600/api/competition/all-competitions'
+    # ADDRESS_COMP = 'http://174.137.53.253:30500/api/competition/all-competitions'
 
 
