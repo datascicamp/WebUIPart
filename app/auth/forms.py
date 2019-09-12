a8deea075d0b3eb53e59b76cf69b661a33e88037
MAC_Herb <hewang@mail.bnu.edu.cn>
2019-09-12 09:33:00 +0800

    Add CSS

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,38 +1,96 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, widgets
 from wtforms.validators import DataRequired, EqualTo, Email
 
 
 # Reset password form
 class ResetPasswordForm(FlaskForm):
-    new_password = PasswordField('Your new Password', validators=[DataRequired()])
-    re_new_password = PasswordField('Entry again', validators=[DataRequired(), EqualTo('new_password')])
-    submit = SubmitField('Reset')
+    new_password = PasswordField("Your new Password", validators=[DataRequired()])
+    re_new_password = PasswordField(
+        "Entry again", validators=[DataRequired(), EqualTo("new_password")]
+    )
+    submit = SubmitField("Reset")
 
 
 # Reset password request form
 class ResetPasswordRequestForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    submit = SubmitField('Request Password Reset')
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Request Password Reset")
 
 
 # Register request form
 class RegisterRequestForm(FlaskForm):
-    submit = SubmitField('Click to Verify Your Email')
+    submit = SubmitField("Click to Verify Your Email")
 
 
 # Login form
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
+    email = StringField(
+        "Email",
+        validators=[DataRequired(), Email()],
+        widget=widgets.TextInput(),
+        render_kw={
+            "class": "form-control",
+            "placeholder": "Enter email",
+            "required": "",
+            "autofocus": "",
+        },
+    )
+    password = PasswordField(
+        "Password",
+        validators=[DataRequired()],
+        widget=widgets.PasswordInput(),
+        render_kw={
+            "class": "form-control",
+            "placeholder": "Password",
+            "required": "",
+            "autofocus": "",
+        },
+    )
+    submit = SubmitField("Login")
 
 
 # Register form
 class RegisterForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    re_password = PasswordField('Entry again', validators=[DataRequired(), EqualTo('password')])
-    captcha = StringField('CAPTCHA', validators=[DataRequired()])
-    submit = SubmitField('Register')
+    email = StringField(
+        "Email",
+        validators=[DataRequired(), Email()],
+        render_kw={
+            "class": "form-control",
+            "placeholder": "Enter your Email",
+            "required": "",
+            "autofocus": "",
+        },
+    )
+    password = PasswordField(
+        "Password",
+        validators=[DataRequired()],
+        render_kw={
+            "class": "form-control",
+            "placeholder": "Enter your password!",
+            "required": "",
+            "autofocus": "",
+        },
+    )
+    re_password = PasswordField(
+        "Confirm",
+        validators=[DataRequired(), EqualTo("password")],
+        render_kw={
+            "class": "form-control",
+            "placeholder": "Enter your password again!",
+            "required": "",
+            "autofocus": "",
+        },
+    )
+    captcha = StringField(
+        "CAPTCHA",
+        validators=[DataRequired()],
+        render_kw={
+            "class": "form-control",
+            "placeholder": "CAPTCHA",
+            "required": "",
+            "autofocus": "",
+        },
+    )
+    submit = SubmitField("Register")
 
