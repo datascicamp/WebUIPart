6d38894de4d3980db044a4b6e25b9ec5f67a5734
XinyaoTian <leontian1024@gmail.com>
2019-09-29 21:01:04 +0800

    /修正了赛题更新页面

--- app/competition/forms.py
+++ app/competition/forms.py
@@ -104,13 +104,57 @@ class CompetitionInsertForm(FlaskForm):
 
 # Competition Updating Form
 class CompetitionUpdateForm(FlaskForm):
-    comp_title = StringField("Competition Title", validators=[DataRequired()])
-    comp_subtitle = StringField("Subtitle")
-    comp_range = StringField("Duration", validators=[DataRequired()])
-    comp_url = StringField("Competition Url", validators=[DataRequired()])
-    comp_description = StringField("Competition Description")
-    comp_host_name = StringField("Competition Hostname", validators=[DataRequired()])
-    comp_host_url = StringField("Competition Host Url", validators=[DataRequired()])
+    # Text Field类型，文本输入框，必填
+    comp_title = StringField(
+        "Competition Title",
+        validators=[DataRequired()],
+        render_kw={
+            "class": "form-control",
+            "placeholder": "Enter Title",
+            "required": "",
+            "autofocus": "",
+        },
+    )
+    comp_subtitle = StringField(
+        "Subtitle",
+        render_kw={
+            "class": "form-control",
+            "placeholder": "(Optional)",
+            "required": "",
+            "autofocus": "",
+        },
+    )
+    comp_range = StringField("Duration", validators=[DataRequired()], render_kw={
+            "class": "form-control",
+            "placeholder": "Ex: 24 July - 1 Aug. 2019",
+            "required": "",
+            "autofocus": "",
+        },)
+    comp_url = StringField("Competition Url", validators=[DataRequired(),],render_kw={
+            "class": "form-control",
+            "placeholder": "URL",
+            "required": "",
+            "autofocus": "",
+        },)
+    # Text Area类型，段落输入框
+    comp_description = TextAreaField("Competition Description", render_kw={
+            "class": "form-control",
+            "placeholder": "(Optional)",
+            "required": "",
+            "autofocus": "",
+        },)
+    comp_host_name = StringField("Competition Hostname", validators=[DataRequired()],render_kw={
+            "class": "form-control",
+            "placeholder": "Ex: Kaggle, 天池, ...",
+            "required": "",
+            "autofocus": "",
+        },)
+    comp_host_url = StringField("Competition Host Url", validators=[DataRequired()],render_kw={
+            "class": "form-control",
+            "placeholder": "URL",
+            "required": "",
+            "autofocus": "",
+        },)
     prize_currency = StringField("Currency Type")
     prize_amount = StringField("Prize")
     # Text Field类型，文本输入框，必须输入是"年-月-日 时:分:秒"格式的日期
@@ -140,6 +184,8 @@ class CompetitionUpdateForm(FlaskForm):
         ],
         validators=[DataRequired()],
     )
+    # comp_scenario  = BooleanField('Competition Scenario', default='checked',
+    #                             validators=[DataRequired()])
     data_feature = StringField("Data Feature", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
