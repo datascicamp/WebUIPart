fc67a180528c7147a3095f34aac9ffa1a2f4467b
MAC_Herb <hewang@mail.bnu.edu.cn>
2019-09-30 16:30:21 +0800

    New NavBar style

--- app/static/js/Nav_Btn.js
+++ app/static/js/Nav_Btn.js
@@ -4,17 +4,21 @@ window.onscroll = function () {
 
 function scrollFunction() {
     // 导航栏  https://www.w3schools.com/howto/howto_js_navbar_slide.asp
+    // https://www.w3schools.com/howto/howto_js_navbar_shrink_scroll.asp
     // When the user scrolls down 20px from the top of the document, slide down the navbar
     // When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
 
     // Scroll Back To Top Button   https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
     // When the user scrolls down 20px from the top of the document, show the button        
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("navbar").style.top = "0";
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        // document.getElementById("navbar").style.top = "0";  // howto_js_navbar_slide
+        document.getElementById("navbar").style.padding = "5px 10px";
+        document.getElementById("logo").style.fontSize = "25px";
         document.getElementById("myBtn").style.display = "block";
     } else {
-        document.getElementById("navbar").style.top = "-60px";
-        //document.getElementById("navbar").style.padding = "0";
+        // document.getElementById("navbar").style.top = "-60px"; // howto_js_navbar_slide
+        document.getElementById("navbar").style.padding = "30px 10px";
+        document.getElementById("logo").style.fontSize = "35px";
         document.getElementById("myBtn").style.display = "none";
     }
 }
