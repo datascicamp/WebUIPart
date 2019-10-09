f2fb8874047b22a56d7605519c20828a6fca1842
iphysresearch <hewang@mail.bnu.edu.cn>
2019-10-09 17:16:49 +0800

    Init after installation

--- webServer.py
+++ webServer.py
@@ -6,3 +6,16 @@ logging.basicConfig(level=logging.INFO)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
+
+
+# Init after installation
+# >> pip install Flask-Login
+# >> pip install flask_bootstrap
+# >> pip install bootstrap-flask
+# >> pip install flask_wtf
+
+# Eir....
+
+
+# Run
+# >> flask run --cert=adhoc
