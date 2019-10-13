a31f9402e0bbb5605582fa21a435ad57e90c1938
iphysresearch <hewang@mail.bnu.edu.cn>
2019-10-13 12:37:49 +0800

    New Style and Hover for compInsert and compUpdate

--- app/competition/forms.py
+++ app/competition/forms.py
@@ -31,19 +31,22 @@ class CompetitionInsertForm(FlaskForm):
         "Subtitle (optional)",
         render_kw={
             "class": "form-control",
-            "placeholder": "Start here! Predict survival on the Titanic and get familiar with ML basics",
+            "data-adg-tooltip-simple": "A qualifying subtitle (optional) is used as an</br>additional and supplementary examination.<br><img src='/static/img/Subtitle.png' >",
+            "placeholder": "Predict survival on the Titanic and get familiar with ML basics",
             "required": "",
             "autofocus": "",
         },
     )
     comp_range = StringField("Duration", validators=[DataRequired()], render_kw={
             "class": "form-control",
+            "data-adg-tooltip-simple": "Specify the time periods as you want it to be! Eg: <br><span class='text-muted'>Now - 30 September, 2018<br>Oct. 1, 2019 - Never<br>Oct 13th, 2019 - maybe Feb, 2020</span><br><img src='/static/img/Duration.png'>",
             "placeholder": "24 July - 1 Aug. 2019",
             "required": "",
             "autofocus": "",
         },)
     comp_url = StringField("Competition Url", validators=[DataRequired(),],render_kw={
             "class": "form-control",
+            "data-adg-tooltip-simple": 'The complete URL of the competition. <span class="text-muted">The URL<br>must include the protocol (<code>http://</code> or <code>https://</code>)<br>to be processed correctly.<br><img src="/static/img/URL.png"></span>',
             "placeholder": "https://www.kaggle.com/c/titanic",
             "required": "",
             "autofocus": "",
@@ -51,7 +54,8 @@ class CompetitionInsertForm(FlaskForm):
     # Text Area类型，段落输入框
     comp_description = TextAreaField("Competition Description (optional)", render_kw={
             "class": "form-control",
-            "placeholder": "In this competition, you’ll gain access to two similar datasets that include passenger information like name, age, gender, socio-economic class, etc. One dataset is titled `train.csv` and the other is titled `test.csv`. \nUsing the patterns you find in the train.csv data, predict whether the other 418 passengers on board (found in test.csv) survived. ",
+            "data-adg-tooltip-simple":'In one or two paragraphs, describe what the task will achieve<br>and describe the data as simply as possible while still<br>capturing their important features.',
+            "placeholder": "In this competition, you’ll gain access to two similar datasets that include passenger information like name, age, gender, socio-economic class, etc. Using the patterns you find in the dataset, predict whether the other 418 passengers on board survived.",
             "required": "",
             "autofocus": "",
             "rows": 3, 
@@ -60,33 +64,41 @@ class CompetitionInsertForm(FlaskForm):
     comp_host_name = StringField("Competition Hostname", validators=[DataRequired()],render_kw={
             "class": "form-control",
             "placeholder": "Kaggle",
+            "style": 'width: 80%;display: inline',
             "required": "",
             "autofocus": "",
         },)
     comp_host_url = StringField("Competition Host Url", validators=[DataRequired()],render_kw={
             "class": "form-control",
             "placeholder": "https://www.kaggle.com",
+            "style": 'width: 80%;display: inline',
             "required": "",
             "autofocus": "",
         },)
     prize_currency = StringField("Prize Type (optional)", render_kw={
             "class": "form-control",
-            "placeholder": "USD / RMB / Swag / ...",
+            "placeholder": "Prize_block2",#,"USD / RMB / Swag / ...",
+            "style": 'width: 30%;display: inline',       
+            "data-adg-tooltip-simple": 'If no prize at all, just leave them empty.<br>Eg: <span class="text-muted"><u>10,000</u> <u>USD</u></span> or <span class="text-muted"><u>Kaggle</u> <u>Swag</u></span><br>Eg: <span class="text-muted"><u>20万</u> <u>RMB</u></span> or <span class="text-muted"><u>Nvidia GTX 1080ti</u> <u>x 4</u></span><br><img src="/static/img/Prize.png">',     
             "required": "",
             "autofocus": "",
         },)
     prize_amount = StringField("Prize (optional)", render_kw={
             "class": "form-control",
-            "placeholder": "10,000,000 / Nvidia GTX 1080ti x 4 / 2 resea. fellowships up to 12 months / ...",
+            "placeholder": "Prize_block1",#,"10,000,000 / Nvidia GTX 1080ti x 4 / 2 resea. fellowships up to 12 months / ...",
+            "style": 'width: 30%;display: inline',
+            "data-adg-tooltip-simple": 'If no prize at all, just leave them empty.<br>Eg: <span class="text-muted"><u>10,000</u> <u>USD</u></span> or <span class="text-muted"><u>Kaggle</u> <u>Swag</u></span><br>Eg: <span class="text-muted"><u>20万</u> <u>RMB</u></span> or <span class="text-muted"><u>Nvidia GTX 1080ti</u> <u>x 4</u></span><br><img src="/static/img/Prize.png">',     
             "required": "",
             "autofocus": "",
         },)
     # Text Field类型，文本输入框，必须输入是"年-月-日 时:分:秒"格式的日期
     deadline = DateTimeField(
-        "Entry Deadline", format="%Y-%m-%d %H:%M:%S", render_kw={
+        "Entry Deadline", format="%Y-%m-%d %H:%M:%S", validators=[DataRequired()],
+        render_kw={
             "class": "form-control js-inline-picker",
             "placeholder": "%s" % (datetime.datetime.today().strftime(format="%Y-%m-%d")+" 23:59:59"),
             "required": "",
+            "style": 'width: 18%;display: inline',
             "autofocus": "",
             "value": "%s" % (datetime.datetime.today().strftime(format="%Y-%m-%d")+" 23:59:59"),
         },
@@ -97,6 +109,9 @@ class CompetitionInsertForm(FlaskForm):
             ("UTC", "UTC"),
             ("Asia/Shanghai", "Asia/Shanghai"),
         ],
+        render_kw={
+            "style": 'height: 37px;display: inline',
+        },
         validators=[DataRequired()],
     )
     comp_scenario = SelectMultipleField(
@@ -111,18 +126,30 @@ class CompetitionInsertForm(FlaskForm):
         description = "多选",
         render_kw={
             "multiple": "multiple",
-            "style": "height: 100px;"
+            "style": "height: 100px;width: 50%;",
+            "data-adg-tooltip-simple": """<b>Data Mining</b>(DM): Usually Structured data or tabular/spreadsheet, with columns and rows that clearly define its attributes.<br>
+                                          <b>Computer Vision</b>(CV): Including image/video processing with matrix/arrays as data structures.<br>
+                                          <b>Natural Language Processing</b>(NLP): Usually natural language-based data like text/speech or word lattices.<br>
+                                          <b>Speech/Signal Proccessing</b>(SP): Specifically, 1-dim non-structured data like signal/music/sound, and even symbolic logic-based data.<br>
+                                          <b>Reinforcement Learning/Robotics</b>(RL): Using the concepts of agents or environments.""",
             },
         validators=[DataRequired()],
     )
     data_feature = SelectMultipleField(
         "Data Feature (multiple choices)", 
         choices=[
-            ("Structured data", "Structured data"),
-            ("1-dim non-structured data", "1-dim non-structured data"),
-            ("2-dim non-structured data", "2-dim non-structured data"),
-            ("3-dim non-structured data", "3-dim non-structured data"),
-        ], 	        
+            ("Structured", "Structured"),
+            ("Non-structured", "Non-structured"),
+            ("Semi-supervised", "Semi-supervised"),
+            ("Supervised", "Supervised"),
+            ("Geographic", "Geographic"),
+            ("Time-series", "Time-series"),
+            ("Natural language-based", "Natural language-based"),
+        ],
+        render_kw={
+            "multiple": "multiple",
+            "style": "height: 140px;width: 50%;",     
+            },               
         validators=[DataRequired()])
     submit = SubmitField("Submit")
 
@@ -144,19 +171,22 @@ class CompetitionUpdateForm(FlaskForm):
         "Subtitle (optional)",
         render_kw={
             "class": "form-control",
-            "placeholder": "Start here! Predict survival on the Titanic and get familiar with ML basics",
+            "data-adg-tooltip-simple": "A qualifying subtitle (optional) is used as an</br>additional and supplementary examination.<br><img src='/static/img/Subtitle.png' >",
+            "placeholder": "Predict survival on the Titanic and get familiar with ML basics",
             "required": "",
             "autofocus": "",
         },
     )
     comp_range = StringField("Duration", validators=[DataRequired()], render_kw={
             "class": "form-control",
+            "data-adg-tooltip-simple": "Specify the time periods as you want it to be! Eg: <br><span class='text-muted'>Now - 30 September, 2018<br>Oct. 1, 2019 - Never<br>Oct 13th, 2019 - maybe Feb, 2020</span><br><img src='/static/img/Duration.png'>",
             "placeholder": "24 July - 1 Aug. 2019",
             "required": "",
             "autofocus": "",
         },)
     comp_url = StringField("Competition Url", validators=[DataRequired(),],render_kw={
             "class": "form-control",
+            "data-adg-tooltip-simple": 'The complete URL of the competition. <span class="text-muted">The URL<br>must include the protocol (<code>http://</code> or <code>https://</code>)<br>to be processed correctly.<br><img src="/static/img/URL.png"></span>',
             "placeholder": "https://www.kaggle.com/c/titanic",
             "required": "",
             "autofocus": "",
@@ -164,7 +194,8 @@ class CompetitionUpdateForm(FlaskForm):
     # Text Area类型，段落输入框
     comp_description = TextAreaField("Competition Description (optional)", render_kw={
             "class": "form-control",
-            "placeholder": "In this competition, you’ll gain access to two similar datasets that include passenger information like name, age, gender, socio-economic class, etc. One dataset is titled `train.csv` and the other is titled `test.csv`. \nUsing the patterns you find in the train.csv data, predict whether the other 418 passengers on board (found in test.csv) survived. ",
+            "data-adg-tooltip-simple":'In one or two paragraphs, describe what the task will achieve<br>and describe the data as simply as possible while still<br>capturing their important features.',
+            "placeholder": "In this competition, you’ll gain access to two similar datasets that include passenger information like name, age, gender, socio-economic class, etc. Using the patterns you find in the dataset, predict whether the other 418 passengers on board survived.",
             "required": "",
             "autofocus": "",
             "rows": 3, 
@@ -173,33 +204,41 @@ class CompetitionUpdateForm(FlaskForm):
     comp_host_name = StringField("Competition Hostname", validators=[DataRequired()],render_kw={
             "class": "form-control",
             "placeholder": "Kaggle",
+            "style": 'width: 80%;display: inline',
             "required": "",
             "autofocus": "",
         },)
     comp_host_url = StringField("Competition Host Url", validators=[DataRequired()],render_kw={
             "class": "form-control",
             "placeholder": "https://www.kaggle.com",
+            "style": 'width: 80%;display: inline',
             "required": "",
             "autofocus": "",
         },)
     prize_currency = StringField("Prize Type (optional)", render_kw={
             "class": "form-control",
-            "placeholder": "USD / RMB / Swag / ...",
+            "placeholder": "Prize_block2",#,"USD / RMB / Swag / ...",
+            "style": 'width: 30%;display: inline',       
+            "data-adg-tooltip-simple": 'If no prize at all, just leave them empty.<br>Eg: <span class="text-muted"><u>10,000</u> <u>USD</u></span> or <span class="text-muted"><u>Kaggle</u> <u>Swag</u></span><br>Eg: <span class="text-muted"><u>20万</u> <u>RMB</u></span> or <span class="text-muted"><u>Nvidia GTX 1080ti</u> <u>x 4</u></span><br><img src="/static/img/Prize.png">',     
             "required": "",
             "autofocus": "",
         },)
     prize_amount = StringField("Prize (optional)", render_kw={
             "class": "form-control",
-            "placeholder": "10,000,000 / Nvidia GTX 1080ti x 4 / 2 resea. fellowships up to 12 months / ...",
+            "placeholder": "Prize_block1",#,"10,000,000 / Nvidia GTX 1080ti x 4 / 2 resea. fellowships up to 12 months / ...",
+            "style": 'width: 30%;display: inline',
+            "data-adg-tooltip-simple": 'If no prize at all, just leave them empty.<br>Eg: <span class="text-muted"><u>10,000</u> <u>USD</u></span> or <span class="text-muted"><u>Kaggle</u> <u>Swag</u></span><br>Eg: <span class="text-muted"><u>20万</u> <u>RMB</u></span> or <span class="text-muted"><u>Nvidia GTX 1080ti</u> <u>x 4</u></span><br><img src="/static/img/Prize.png">',     
             "required": "",
             "autofocus": "",
         },)
     # Text Field类型，文本输入框，必须输入是"年-月-日 时:分:秒"格式的日期
     deadline = DateTimeField(
-        "Entry Deadline", format="%Y-%m-%d %H:%M:%S", render_kw={
+        "Entry Deadline", format="%Y-%m-%d %H:%M:%S", validators=[DataRequired()],
+        render_kw={
             "class": "form-control js-inline-picker",
             "placeholder": "%s" % (datetime.datetime.today().strftime(format="%Y-%m-%d")+" 23:59:59"),
             "required": "",
+            "style": 'width: 18%;display: inline',
             "autofocus": "",
             "value": "%s" % (datetime.datetime.today().strftime(format="%Y-%m-%d")+" 23:59:59"),
         },
@@ -210,6 +249,9 @@ class CompetitionUpdateForm(FlaskForm):
             ("UTC", "UTC"),
             ("Asia/Shanghai", "Asia/Shanghai"),
         ],
+        render_kw={
+            "style": 'height: 37px;display: inline',
+        },
         validators=[DataRequired()],
     )
     comp_scenario = SelectMultipleField(
@@ -224,18 +266,30 @@ class CompetitionUpdateForm(FlaskForm):
         description = "多选",
         render_kw={
             "multiple": "multiple",
-            "style": "height: 100px;"
+            "style": "height: 100px;width: 50%;",
+            "data-adg-tooltip-simple": """<b>Data Mining</b>(DM): Usually Structured data or tabular/spreadsheet, with columns and rows that clearly define its attributes.<br>
+                                          <b>Computer Vision</b>(CV): Including image/video processing with matrix/arrays as data structures.<br>
+                                          <b>Natural Language Processing</b>(NLP): Usually natural language-based data like text/speech or word lattices.<br>
+                                          <b>Speech/Signal Proccessing</b>(SP): Specifically, 1-dim non-structured data like signal/music/sound, and even symbolic logic-based data.<br>
+                                          <b>Reinforcement Learning/Robotics</b>(RL): Using the concepts of agents or environments.""",
             },
         validators=[DataRequired()],
     )
     data_feature = SelectMultipleField(
         "Data Feature (multiple choices)", 
         choices=[
-            ("Structured data", "Structured data"),
-            ("1-dim non-structured data", "1-dim non-structured data"),
-            ("2-dim non-structured data", "2-dim non-structured data"),
-            ("3-dim non-structured data", "3-dim non-structured data"),
-        ], 	        
+            ("Structured", "Structured"),
+            ("Non-structured", "Non-structured"),
+            ("Semi-supervised", "Semi-supervised"),
+            ("Supervised", "Supervised"),
+            ("Geographic", "Geographic"),
+            ("Time-series", "Time-series"),
+            ("Natural language-based", "Natural language-based"),
+        ],
+        render_kw={
+            "multiple": "multiple",
+            "style": "height: 140px;width: 50%;",     
+            },               
         validators=[DataRequired()])
     submit = SubmitField("Submit")
 
---
+++ app/static/comp_hover/comp_hover.js
@@ -0,0 +1,107 @@
+;(function () {
+    var AdgTooltipSimple
+  
+    AdgTooltipSimple = class AdgTooltipSimple {
+      constructor (el) {
+        this.$el = $(el)
+        this.value = this.$el.attr('data-adg-tooltip-simple')
+        this.$el.attr('data-adg-tooltip-simple', null)
+        this.initContainer()
+        this.attachContentToEl()
+        this.initElEvents()
+        this.initContainerEvents()
+      }
+  
+      initContainer () {
+        this.$container = $(
+          "<span class='adg-tooltip-simple' aria-hidden='true'></span>"
+        )
+        this.$el.before(this.$container)
+        this.initIcon()
+        return this.initBalloon()
+      }
+  
+      initIcon () {
+        // Set focusable="false" for IE, see https://stackoverflow.com/questions/18646111/disable-onfocus-event-for-svg-element
+        this.$icon = $(
+          //"<span class='adg-tooltip-simple-icon'><svg class='icon' focusable='false'><use xlink:href='#tooltip' /></svg></span>"
+          ""
+        )
+        return this.$container.append(this.$icon)
+      }
+  
+      initBalloon () {
+        this.$balloon = $(
+          `<span class='adg-tooltip-simple-balloon' hidden>${this.value}</span>`
+        )
+        this.$balloon.attr('id', `${this.$el.attr('id')}-balloon`)
+        return this.$container.append(this.$balloon)
+      }
+  
+      attachContentToEl () {
+        var valueElement
+        valueElement = $(
+          `<span class='adg-visually-hidden'> (${this.value})</span>`
+        )
+        if (this.$el.is('input, textarea, select')) {
+          return $(`label[for='${this.$el.attr('id')}'`).append(valueElement)
+        } else {
+          return this.$el.append(valueElement)
+        }
+      }
+  
+      initElEvents () {
+        this.$el.focusin(() => {
+          return this.show()
+        })
+        this.$el.focusout(() => {
+          if (!this.$container.is(':hover')) {
+            return this.hide()
+          }
+        })
+        this.$el.mouseenter(() => {
+          return this.show()
+        })
+        this.$el.mouseleave(() => {
+          if (!this.$el.is(':focus')) {
+            return this.hide()
+          }
+        })
+        return this.$el.keyup(e => {
+          if (e.keyCode === 27) {
+            // Esc
+            if (this.$balloon.is(':visible')) {
+              return this.hide()
+            } else {
+              return this.show()
+            }
+          }
+        })
+      }
+  
+      initContainerEvents () {
+        this.$container.mouseenter(() => {
+          return this.show()
+        })
+        return this.$container.mouseleave(() => {
+          if (!this.$el.is(':focus')) {
+            return this.hide()
+          }
+        })
+      }
+  
+      show () {
+        return this.$balloon.attr('hidden', false)
+      }
+  
+      hide () {
+        return this.$balloon.attr('hidden', true)
+      }
+    }
+  
+    $(document).ready(function () {
+      return $('[data-adg-tooltip-simple]').each(function () {
+        return new AdgTooltipSimple(this)
+      })
+    })
+  }.call(this))
