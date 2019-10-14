612da9f24f316f5fb49595c0279e19884768f616
iphysresearch <hewang@mail.bnu.edu.cn>
2019-10-14 13:40:26 +0800

    Add mailing list!

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
 from app import app
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, Response
 from config import Config
 from func_pack import get_api_info
 from flask_login import logout_user
 from app.models import User
 import requests
 import datetime
+import numpy as np
+import pytz, heapq # 堆队列
 
 
 # id_type_checkboxs = [
@@ -36,7 +38,7 @@ Filtering_hashcomp = lambda comps, hash: [ comp for comp in comps if comp['comp_
 
 def nonFiltering_deadline(comp):
     if comp['deadline']:
-        print(type(comp['deadline']), comp['deadline'])
+        # print(type(comp['deadline']), comp['deadline'])
         return int(''.join(comp['deadline'].split()[0].split('-')))>=int(datetime.datetime.today().strftime('%Y%m%d'))
     else:
         comp['deadline'] = 'No deadline'  # Working for empty in comp['deadline']
@@ -70,21 +72,85 @@ def logout_func():
 def about():
     return render_template('about.html')
 
-# @app.route("/competition=<comp_record_hash>", )
-# def comp(comp_record_hash):
-#     addr = 'http://' + Config.COMPETITION_SERVICE_URL + '/api/competition/all-competitions'
-#     info_list = get_api_info(requests.get(addr))
-#     info_list = Filtering_hashcomp(info_list, comp_record_hash) # Filtering the list by comp_record_hash
-#     assert len(info_list) == 1
-#     print(info_list)
-#     return render_template(
-#         "competition.html",
-#         Comp=info_list[0],
-#         type_dict=type_dict,
-#     )
 
-# @app.route("/hostby.html")
-# def hostby(id_type_checkboxs=id_type_checkboxs, hosts=hosts):
-#     return render_template(
-#         "hostby.html", id_type_checkboxs=id_type_checkboxs, hosts=hosts
-#     )
+# XML
+
+@app.route("/update_log.xml")
+def products_xml():
+
+    addr = 'http://' + Config.COMPETITION_SERVICE_URL + '/api/competition/all-competitions'
+    info_list = get_api_info(requests.get(addr))
+    competitions = sorted(Filtering_pastcomp(info_list), key=Func_deadline) # Filtering and sort the list by deadline
+
+
+    # num_largest = 2
+    # comps_pubtime = np.array([int(i["pubtime"].replace("-", "")) for i in competitions])
+    # comps_pubtime = np.array([int(comp['publish_time'][:10].replace("-", "")) for comp in competitions])
+    comps_pubtime = np.array([comp['publish_time'] for comp in competitions])
+    valid_index = [datetime.datetime.strptime(pubtime, '%Y-%m-%d %H:%M:%S') < (datetime.datetime.today()-datetime.timedelta(days=7)) for pubtime in comps_pubtime]
+    competitions = np.array(competitions)[np.array(valid_index)] #  Filtering the comps for a week
+
+
+
+    # time_largest = heapq.nlargest(num_largest, np.unique(comps_pubtime))
+    # index_largest = [
+    #     np.where(comps_pubtime == largest_value)[0].tolist()
+    #     for largest_value in time_largest
+    # ]
+    # competitions = [
+    #     (competitions[largest_index], block_time)
+    #     for block_time, largest_time in enumerate(index_largest)
+    #     for largest_index in largest_time
+    # ]
+
+    # [
+    #     comp.update(
+    #         {
+    #             "pubtime": datetime.datetime.fromtimestamp(
+    #                 int(
+    #                     datetime.datetime.strptime(
+    #                         comp["pubtime"], "%Y-%m-%d"
+    #                     ).timestamp()
+    #                 ),
+    #                 pytz.timezone("Asia/Shanghai"),
+    #             ).strftime("%a, %d %b %Y")
+    #         }
+    #     )
+    #     for comp, _ in competitions
+    # ]
+
+    output = '<?xml version="1.0" encoding="UTF-8" ?>'
+    output += '<rss version="2.0">'
+    output += "<channel>"
+    output += "<title>DataSciCamp: Data Science Challenge / Competition</title>"
+    output += "<link>https://www.datascicamp.com/</link>"
+    output += "<description>Update within the last 7 days!</description>"
+    # update_block = [
+    #     datetime.datetime.fromtimestamp(
+    #         int(
+    #             datetime.datetime.strptime(
+    #                 str(time_largest[block]), "%Y%m%d"
+    #             ).timestamp()
+    #         ),
+    #         pytz.timezone("Asia/Shanghai"),
+    #     ).strftime("%m/%d/%Y")
+    #     for block in range(num_largest)
+    # ]
+    # output += "<description>Latest update at {} (GMT+0800).</description>".format(
+    #     update_block[0]
+    # )
+
+    for comp in competitions:
+        output += "<item>"
+        output += "<title>{:s}</title>".format(comp["comp_title"])
+        output += "<link>{:s}</link>".format("https://www.datascicamp.com/competition-operator/competition-detail/"+comp["comp_record_hash"])
+        # output += "<category>{:s}</category>".format("/".join(comp["type1"]))
+        output += "<category>{:s}</category>".format("/".join(comp["comp_scenario"]))
+        output += "<pubDate>{:s}</pubDate>".format(comp["publish_time"])
+        output += "<description>{:s}</description>".format(
+            comp["comp_description"].replace("<br>", "")
+        )
+        output += "</item>"
+    output += "</channel>"
+    output += "</rss>"
+    return Response(output, mimetype="application/xml")
